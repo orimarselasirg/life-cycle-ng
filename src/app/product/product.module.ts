b93810daa00769c76800576a13d4404733d045f8
Ramiro Grisales <ramiro.grisales@centrohub.co>
2024-07-04 16:33:31 -0500

    first commit

---
+++ src/app/product/product.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductRoutingModule } from './product-routing.module';
+import { ProductComponent } from './pages/product/product.component';
+import { PriceComponent } from './components/price/price.component';
+
+
+@NgModule({
+  declarations: [
+    ProductComponent,
+    PriceComponent
+  ],
+  imports: [
+    CommonModule,
+    ProductRoutingModule
+  ]
+})
+export class ProductModule { }
